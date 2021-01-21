a0c27745b298d4bee9701794b39947e7f8996ed8
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-21 15:51:41 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrintModelKegBeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            float currVolumeKeg = 0.0F;
+            float maxValume = float.MinValue;
+            string biggestKeg = string.Empty;
+           
+
+            for (int i = 1; i <= nLines; i++)
+            {
+                string modelKeg = Console.ReadLine();
+                float radiusKeg = float.Parse(Console.ReadLine());
+                int heightKeg = int.Parse(Console.ReadLine());
+                currVolumeKeg = (float)(radiusKeg * heightKeg * Math.PI);
+
+                if (currVolumeKeg > maxValume)
+                {
+                    maxValume = currVolumeKeg;
+                    biggestKeg = modelKeg;
+                   
+                }               
+            }
+                Console.WriteLine(biggestKeg);
+                
+            
+            
+        }
+    }
+}
